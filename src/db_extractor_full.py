0b2b579582f9cae027e90a4ed3f5366921232481
cameroncaci <cameron.jewell@caci.com>
2024-10-30 16:13:19 -0400

    initial refactor for multi part uploading

--- src/db_extractor_full.py
+++ src/db_extractor_full.py
@@ -1,12 +1,12 @@
 import boto3
 import pg8000
 import json
-import time
+import io
 import db_conn
 import os
 from uuid import UUID
 import datetime
-
+import gc
 
 # Declare the global connection object to use during warm starting
 # to reuse connections that were established during a previous invocation.
@@ -16,7 +16,11 @@ connection = None
 # know for the next run which time to select from
 current_run_time = datetime.datetime.now()
 
-#Class to format json UUID's
+# Define batch size fetching of records for json dumps
+batch_size = 100000
+
+
+# Class to format json UUID's
 class UUIDEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, UUID):
@@ -25,38 +29,150 @@ class UUIDEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+#  Helper function to batch fetch data and use multipart uploading with S3
+def fetch_and_upload_cursor_results(
+    cursor: pg8000.Cursor, bucket_name, key_name, column_names
+):
+    s3_client = boto3.client("s3")
+    # Initiate multipart upload
+    multipart_upload = s3_client.create_multipart_upload(
+        Bucket=bucket_name,
+        Key=key_name,
+        ServerSideEncryption="AES256",
+    )
+    upload_id = multipart_upload["UploadId"]
+    parts = []
+    part_number = 1
+    buffer = io.BytesIO()
+    buffer_size = 0
+    min_part_size = 5 * 1024 * 1024  # 5 MB
+
+    try:
+        # Fetch the results of the cursor query
+        for batch in fetch_batches(cursor):
+            data_with_col_names = (
+                map_row_to_columns(row, column_names) for row in batch
+            )
+            for record in data_with_col_names:
+                json_line = json.dumps(record, cls=UUIDEncoder, default=str) + "\n"
+                json_line_bytes = json_line.encode("utf-8")
+                buffer.write(json_line_bytes)
+                buffer_size += len(json_line_bytes)
+
+                # Upload part if buffer reaches the minimum part size of 5MB
+                if buffer_size >= min_part_size:
+                    # This part is now finished being appended too
+                    # Trigger upload, reset buffer, and proceed with the next one
+                    buffer.seek(0)
+                    response = s3_client.upload_part(
+                        Bucket=bucket_name,
+                        Key=key_name,
+                        PartNumber=part_number,
+                        UploadId=upload_id,
+                        Body=buffer,
+                    )
+                    parts.append({"PartNumber": part_number, "ETag": response["ETag"]})
+                    part_number += 1
+                    buffer.close()
+                    buffer = io.BytesIO()
+                    buffer_size = 0
+
+                    gc.collect()
+
+        # Upload any remaining data in the buffer (Eg, the last batch finished with a part < 5MB)
+        if buffer_size > 0:
+            # Final part upload
+            buffer.seek(0)
+            response = s3_client.upload_part(
+                Bucket=bucket_name,
+                Key=key_name,
+                PartNumber=part_number,
+                UploadId=upload_id,
+                Body=buffer,
+            )
+            parts.append({"PartNumber": part_number, "ETag": response["ETag"]})
+            buffer.close()
+
+        if parts:
+            # Complete multipart upload
+            s3_client.complete_multipart_upload(
+                Bucket=bucket_name,
+                Key=key_name,
+                UploadId=upload_id,
+                MultipartUpload={"Parts": parts},
+            )
+            print(
+                f"Data Warehouse Lambda - INFO - DB Extract - Successfully wrote {bucket_name}/{key_name}"
+            )
+        else:
+            # Abort the multipart upload if no parts were uploaded
+            s3_client.abort_multipart_upload(
+                Bucket=bucket_name, Key=key_name, UploadId=upload_id
+            )
+            print(
+                f"Data Warehouse Lambda - INFO - No data to upload for table {key_name}"
+            )
+
+    except Exception as e:
+        # Abort multipart upload in case of failure
+        s3_client.abort_multipart_upload(
+            Bucket=bucket_name, Key=key_name, UploadId=upload_id
+        )
+        print(
+            f"Data Warehouse Lambda - ERROR - DB Extract - Error during multipart upload: {e}"
+        )
+        raise e
+
+
+# Helper func to yield results rather than return
+# to boost processing efficiency
+def fetch_batches(cursor: pg8000.Cursor):
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            # No more batch results
+            break
+        yield batch
+
+
+def map_row_to_columns(row, column_names):
+    return {column_names[i]: row[i] for i in range(len(column_names))}
+
+
 def db_extractor():
     # Use the get_parameter method of the SSM client to retrieve the parameter
     try:
         # Create an SSM client
-        ssm_client = boto3.client('ssm')
+        ssm_client = boto3.client("ssm")
         response = ssm_client.get_parameter(
-            Name=os.environ['parameter_name'],
-            WithDecryption=True
+            Name=os.environ["parameter_name"], WithDecryption=True
         )
         # The value of the parameter is stored in the 'Value' field of the response
-        parameter_value = response['Parameter']['Value']
+        parameter_value = response["Parameter"]["Value"]
         json_parameter_value = json.loads(parameter_value)
     except Exception as e:
-        print("Data Warehouse Lambda - ERROR - DB Extract - Failed to retrieve values from SSM" + str(e))
-
+        print(
+            "Data Warehouse Lambda - ERROR - DB Extract - Failed to retrieve values from SSM"
+            + str(e)
+        )
 
     global connection
     try:
         if connection is None:
             connection = db_conn.get_connection()
         if connection is None:
-            print("Data Warehouse Lambda - ERROR - DB Extract - Failed to connect to database")
+            print(
+                "Data Warehouse Lambda - ERROR - DB Extract - Failed to connect to database"
+            )
+            return
 
         # Instantiate the cursor object
-        cursor = connection.cursor()
-
-        # Initialize an empty dictionary to store the data
-        data = {}
+        cursor = connection.cursor()  # type: ignore (Type none is handled)
 
         # Get a list of all tables in the database
-        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-        #tmp_tables = cursor.fetchall()
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        )
         tables_tmp = cursor.fetchall()
 
         # Sanitize the table name's
@@ -64,127 +180,126 @@ def db_extractor():
         for tmp_table_name in tables_tmp:
             tmp_table_name = tmp_table_name[0]
             # remove any special characters or whitespaces
-            tmp_table_name = ''.join(e for e in tmp_table_name if e.isalnum() or e == '_')
+            tmp_table_name = "".join(
+                e for e in tmp_table_name if e.isalnum() or e == "_"
+            )
             # make sure the table name is lowercase
             tmp_table_name = tmp_table_name.lower()
             # add the sanitized table name to the list
             tables_list.append(tmp_table_name)
         tables = tuple(tables_list)
 
+        # List tables that should be excluded from the dump
+        table_dump_ignore = [
+            "zip3_distances",
+            "transportation_service_provider_performances",
+            "move",
+            "move_to_gbloc",
+            "archived_access_codes",
+            "schema_migration",
+            "audit_history_tableslist",
+        ]
 
-#        table_dump_ignore = ['django_migrations', 'audit_history', 'archived_access_codes', 'schema_migration', 'audit_history_tableslist', 'awsdms_ddl_audit']
-        table_dump_ignore = ['zip3_distances', 'transportation_service_provider_performances','move' ,'move_to_gbloc' , 'archived_access_codes', 'schema_migration', 'audit_history_tableslist']
-        #larger tables handle differently
-        table_size_large = ['audit_history','transportation_accounting_codes']
+        # S3 bucket upload location
+        bucket_name = os.environ["bucket_name"]
 
-        # For each table
+        # Loop over each table and dump its data. Handling initial dump and incremental dumps on reruns
         for table in tables:
+            s3_key = f"db_data/{str(json_parameter_value['data']['serialNumber'] + 1).zfill(6)}/{table_name}.json"
+
             table_name = table
             # get a list of the table's columns
-            cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name=%s", (table_name,))
+            cursor.execute(
+                "SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name=%s",
+                (table_name,),
+            )
             column_names = list(cursor.fetchall())
 
             # Determine whether we have the timestamp fields created_at and updated_at
             found_created_at = False
             found_updated_at = False
             for column in column_names:
-                if 'updated_at' in column:
+                if "updated_at" in column:
                     found_updated_at = True
-                if 'created_at' in column:
+                if "created_at" in column:
                     found_created_at = True
-
-            # By default we will want to write our data to S3, on error we will skip this
-            write_to_s3 = True
 
             # Set the statement timeout to 600 seconds for this session
             cursor.execute("SET statement_timeout = '600s'")
 
             # If we ignore the table, we do nothing
             if str(table_name) in table_dump_ignore:
-                print("Data Warehouse Lambda - INFO - DB Extract - Didn't extract data for table(ignore list): " + str(table_name))
-            # If we have neither timestamp field, we do a full dump     
-            elif str(table_name) in table_size_large:
-                print("Data Warehouse Lambda - INFO - DB Extract - Performing full dump on " + str(table_name))                
-                cursor.execute("SELECT * FROM " + str(table_name))
-                chunk_size = 800000
-                file_increment = 1
-                while True:
-                    results = cursor.fetchmany(chunk_size)
-                    if not results:
-                        break                                         
-                    column_names = [desc[0] for desc in cursor.description]
-                    data_with_col_names = [{column_names[i]: row[i] for i in range(len(column_names))} for row in results] 
-                    json_data = json.dumps(data_with_col_names, cls=UUIDEncoder, default=str)                        
-                    if write_to_s3 == True:
-                        try:
-                            s3 = boto3.client('s3')
-                            if file_increment == 1:
-                                s3.put_object(Bucket=os.environ['bucket_name'], Key="db_data" + "/" + str(json_parameter_value['data']['serialNumber'] + 1).zfill(6) + "/" + table_name + ".json", Body=json_data, ServerSideEncryption='AES256')
-                                print('Data Warehouse Lambda - INFO - DB Large Extract - Successfully wrote ' + os.environ['bucket_name'] + "/" + "db_data/" + "/"+str(json_parameter_value['data']['serialNumber'] + 1).zfill(6)+"/" + table_name + ".json")
-                            else:    
-                                s3.put_object(Bucket=os.environ['bucket_name'], Key="db_data" + "/" + str(json_parameter_value['data']['serialNumber'] + 1).zfill(6) + "/" + table_name + "_" + str(file_increment) + ".json", Body=json_data, ServerSideEncryption='AES256')
-                                print('Data Warehouse Lambda - INFO - DB Large Extract - Successfully wrote ' + os.environ['bucket_name'] + "/" + "db_data/" + "/"+str(json_parameter_value['data']['serialNumber'] + 1).zfill(6)+"/" + table_name + "_" + str(file_increment) + ".json")
-                            file_increment += 1                            
-                        except Exception as e:
-                            print("Data Warehouse Lambda - ERROR - DB Large Extract - Error writing to S3" + str(e))   
-                            write_to_s3 = False                
-                            break
-                write_to_s3 = False                
+                print(
+                    "Data Warehouse Lambda - INFO - DB Extract - Didn't extract data for table(ignore list): "
+                    + str(table_name)
+                )
+            # Handle if the table being iterated on does not have updated_at or created_at
+            # Since we do not have timestamps to compare to, we must full dump the table without updated or created at
             elif found_updated_at == False and found_created_at == False:
-                print("Data Warehouse Lambda - INFO - DB Extract - Performing full dump on " + str(table_name))                
+                print(
+                    "Data Warehouse Lambda - INFO - DB Extract - Performing full dump on "
+                    + str(table_name)
+                )
+                # Tell the database to execute this query, we will ingest it in chunks
                 cursor.execute("SELECT * FROM " + str(table_name))
-                table_data = cursor.fetchall()
-                column_names = [desc[0] for desc in cursor.description]
-                data_with_col_names = [{column_names[i]: row[i] for i in range(len(column_names))} for row in table_data]
-                json_data = json.dumps(data_with_col_names, cls=UUIDEncoder, default=str)
+                # Fetch cursor results and upload to S3
+                fetch_and_upload_cursor_results(
+                    cursor, bucket_name, s3_key, column_names
+                )
             # If we have created_at but no updated_at, we dump based only on created_at
             elif found_updated_at == False and found_created_at == True:
-                last_run_time = json_parameter_value['data']['lastRunTime']
-                cursor.execute("SELECT * FROM " + str(table_name) + " where (created_at > '" + str(last_run_time) + "') order by created_at;")
-                table_data = cursor.fetchall()
-                column_names = [desc[0] for desc in cursor.description]
-                data_with_col_names = [{column_names[i]: row[i] for i in range(len(column_names))} for row in table_data]
-                json_data = json.dumps(data_with_col_names, cls=UUIDEncoder, default=str)
+                last_run_time = json_parameter_value["data"]["lastRunTime"]
+                cursor.execute(
+                    "SELECT * FROM "
+                    + str(table_name)
+                    + " where (created_at > '"
+                    + str(last_run_time)
+                    + "') order by created_at;"
+                )
+                fetch_and_upload_cursor_results(
+                    cursor, bucket_name, s3_key, column_names
+                )
             # If we have created_at and updated_at, we dump based on both
             elif found_updated_at == True and found_created_at == True:
-                last_run_time = json_parameter_value['data']['lastRunTime']
-                cursor.execute("SELECT * FROM " + str(table_name) + " where ((created_at > %s) OR (updated_at > %s)) order by created_at;", (last_run_time, last_run_time))
-                table_data = cursor.fetchall()
-                column_names = [desc[0] for desc in cursor.description]
-                data_with_col_names = [{column_names[i]: row[i] for i in range(len(column_names))} for row in table_data]
-                json_data = json.dumps(data_with_col_names, cls=UUIDEncoder, default=str)
+                last_run_time = json_parameter_value["data"]["lastRunTime"]
+                cursor.execute(
+                    "SELECT * FROM "
+                    + str(table_name)
+                    + " where ((created_at > %s) OR (updated_at > %s)) order by created_at;",
+                    (last_run_time, last_run_time),
+                )
+                fetch_and_upload_cursor_results(
+                    cursor, bucket_name, s3_key, column_names
+                )
             else:
-                print('Data Warehouse Lambda - ERROR - DB Extract - ' + str(table_name) + ' does not match any criteria for data warehousing')
-                write_to_s3 = False
-
-            # Write the data to S3
-            if write_to_s3 == True:
-                try:
-                    s3 = boto3.client('s3')
-                    s3.put_object(Bucket=os.environ['bucket_name'], Key="db_data" + "/" + str(json_parameter_value['data']['serialNumber'] + 1).zfill(6) + "/" + table_name + ".json", Body=json_data, ServerSideEncryption='AES256')
-                    print('Data Warehouse Lambda - INFO - DB Extract - Successfully wrote ' + os.environ['bucket_name'] + "/" + "db_data/" + "/"+str(json_parameter_value['data']['serialNumber'] + 1).zfill(6)+"/" + table_name + ".json")
-                except Exception as e:
-                    print("Data Warehouse Lambda - ERROR - DB Extract - Error writing to S3" + str(e))
-
-       # Create an SSM client
-        try:
-            ssm_client = boto3.client('ssm')
-            json_parameter_value['data']['serialNumber'] += 1
-            json_parameter_value['data']['lastRunTime'] = str(current_run_time)
-            ssm_client.put_parameter(
-                    Name=os.environ['parameter_name'],
-                    Value=json.dumps(json_parameter_value),
-                    Type='SecureString',
-                    Overwrite=True
+                print(
+                    "Data Warehouse Lambda - ERROR - DB Extract - "
+                    + str(table_name)
+                    + " does not match any criteria for data warehousing"
                 )
 
-            #print("Data Warehouse Lambda - INFO - DB Extract - Updated tracking in SSM")
+        # Create an SSM client
+        try:
+            ssm_client = boto3.client("ssm")
+            json_parameter_value["data"]["serialNumber"] += 1
+            json_parameter_value["data"]["lastRunTime"] = str(current_run_time)
+            ssm_client.put_parameter(
+                Name=os.environ["parameter_name"],
+                Value=json.dumps(json_parameter_value),
+                Type="SecureString",
+                Overwrite=True,
+            )
+
+            print("Data Warehouse Lambda - INFO - DB Extract - Updated tracking in SSM")
         except Exception as e:
-            print("Data Warehouse Lambda - ERROR - DB Extract - Error writing to SSM" + str(e))
+            print(
+                "Data Warehouse Lambda - ERROR - DB Extract - Error writing to SSM"
+                + str(e)
+            )
 
     except Exception as e:
         try:
-            connection.close()
+            connection.close()  # type: ignore (Type none is handled)
         except Exception as e:
             connection = None
         print("Data Warehouse Lambda - ERROR - DB Extract - Failed due to :" + str(e))
